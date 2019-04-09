28c5a21116200e2f6ea51ef41236c340cf439403
Vijendra <vijendra.bhatnagar@stryker.com>
2019-04-09 17:42:13 +0530

    Second Commit

--- src/main/java/CheckActivity/FeatureActivity/BaseClass.java
+++ src/main/java/CheckActivity/FeatureActivity/BaseClass.java
@@ -1,8 +1,12 @@
 package CheckActivity.FeatureActivity;
 
+import java.io.PrintStream;
+
 public class BaseClass {
 
 	public static void main(String args []) {
+		
+		PrintStream sysOutClean = System.out;
 		
 	}
 	
